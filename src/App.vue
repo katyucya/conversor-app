7ffbeef8493f7e117aa99c1d136dcd6c1207c79c
katyucya.vanoni <katyucyafabiane@gmail.com>
2021-06-22 09:14:43 -0400

    Versionamento

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="cifrao-logo" src="/images/sifrao-de-dinheiro.png" alt="vue logo" />
+  <h1>Conversor de Reais para dólares</h1>
+
+  <div class="div-convensor">
+    <span class="p-float-label">
+      <InputText id="input-reais" type="text" v-model="reais" v-bind:disabled="conversor" />
+      <label for="input-reais">Quantidade em Reais</label>
+    </span>
+  </div>
+
+  <div class="div-convensor">
+    <span class="p-float-label">
+      <InputText id="input-dolar" type="text" v-model="dolar" v-bind:disabled="conversor" />
+      <label for="input-dolar">Cotação do Dólar</label>
+    </span>
+  </div>
+
+  <div class="div-convensor" v-if="!conversor">
+    <Button label="Limpar" @click="clear" />
+    <Button label="Calcular" @click="calculate" class="button-calculate" />
+  </div>
+
+  <div v-if="conversor">
+    <p class="label-result">
+      Com {{ reais }} é possível comprar {{ conversor }} dólares a
+      {{ dolar }} cada.
+    </p>
+    <Button @click="clear" label="Calcular novamente" />
+  </div>
+
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      reais: null,
+      dolar: null,
+      conversor: null
+    };
+  },
+  methods: {
+    calculate: function() {
+      this.conversor = (this.reais / this.dolar).toFixed(2);
+    },
+    clear() {
+      this.reais = null;
+      this.dolar = null;
+      this.conversor = null;
+      this.result = "";
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,6 +62,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.cifrao-logo {
+  margin-right: 50px;
+}
+.p-float-label {
+  display: inline-block !important;
+}
+.div conversor {
+  margin-top: 2rem;
+}
+.button-calculate {
+  margin-left: 2rem !important;
+  margin-top: 2rem !important;
+}
+.label-result {
+  font-size: 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+
+const app = createApp(App);
+// registrando na instancia do vue
+app.component('Button', Button);
+app.component('InputText', InputText);
+
+app.use(PrimeVue);
+
+app.mount('#app');
